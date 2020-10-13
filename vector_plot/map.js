f4ec92a7d2c4c22e6109c60561b0dc118f4f8a4d
hannahjordanUM <hm1.jordan@gmail.com>
2020-10-13 17:42:45 -0600

    added

---
+++ vector_plot/map.js
@@ -0,0 +1,150 @@
+
+function load() {
+
+var width = 950;
+var height = 650;
+
+
+
+var svg = d3.select("#map")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height);
+
+var Perc_Infected = d3.map();
+var State = d3.map();
+var County = d3.map();
+var Cases = d3.map();
+var Population = d3.map();
+var path = d3.geoPath();
+var lowColor = 'rgb(245, 245, 255)'; // teal hued
+var highColor = '#006666';
+//var highColor = '#000080'; // purple
+
+/*
+var color = d3.scaleQuantize([2, 18], 
+      d3.schemeOranges[9])
+*/
+var color = d3.scaleLinear().domain([0,16]).range([lowColor, highColor])
+
+
+var g = svg.append("g");
+
+    // add a legend
+    var w = 20, h = 200;
+/*
+    var key = d3.select("svg")
+      .append("svg")
+      .attr("width", w)
+      .attr("height", h)
+      .attr("class", "legend");
+*/
+    var legend = svg.append("defs")
+      .append("svg:linearGradient")
+      .attr("id", "gradient")
+      .attr("x1", "100%")
+      .attr("y1", "0%")
+      .attr("x2", "100%")
+      .attr("y2", "100%")
+      .attr("spreadMethod", "pad");
+
+    legend.append("stop")
+      .attr("offset", "0%")
+      .attr("stop-color", highColor)
+      .attr("stop-opacity", 1);
+
+    legend.append("stop")
+      .attr("offset", "100%")
+      .attr("stop-color", lowColor)
+      .attr("stop-opacity", 1);
+
+    svg.append("rect")
+      .attr("width", w )
+      .attr("height", h+1)
+      .style("fill", "url(#gradient)")
+      .attr("transform", "translate(850,50) rotate(90)");
+
+    var yScale = d3.scaleLinear()
+      .range([h, 0])
+      .domain([0, 16]);
+
+    const ticksAmount = 2;
+    const tickStep = (16 - 0) / (ticksAmount);
+    const step = Math.ceil(tickStep);
+    var yAxis = d3.axisRight(yScale)
+        .ticks(ticksAmount)
+        .tickValues(d3.range(0, 16 + step, step));
+
+
+    //var yAxis = d3.axisRight(y).tickValues(d3.range(0, 0 + 8, 16));
+
+
+    svg.append("g")
+      .attr("transform", "translate(850,70) rotate(90)")
+      .call(yAxis)
+        .selectAll("text")
+        .style("text-anchor","end")
+        .attr("dx","-.6em")
+        .attr("dy","1.5em")
+        .attr("transform","rotate(-90)");
+
+
+var promises = [
+  d3.json("https://raw.githubusercontent.com/hannahjordanUM/Data_Vis_Repo/master/VectorPlot/counties-albers-10m.json"),
+  d3.csv("https://raw.githubusercontent.com/hannahjordanUM/Data_Vis_Repo/master/VectorPlot/covid_confirmed_usafacts_updated.csv", function(d) {
+        Perc_Infected.set(d.countyFIPS,d.cases_per_pop); 
+        State.set(d.countyFIPS,d.State);
+        County.set(d.countyFIPS,d.County_Name);
+        Cases.set(d.countyFIPS,d.num_cases);
+        Population.set(d.countyFIPS,d.population);
+    }),
+
+]
+
+
+Promise.all(promises).then(ready)
+
+function ready([us]) {
+
+
+  svg.append("g")
+      .attr("class", "counties")
+    .selectAll("path")
+    .data(topojson.feature(us, us.objects.counties).features)
+    .enter().append("path")
+      .attr("fill", function(d) { 
+        return color(d.cases_per_pop = Perc_Infected.get(d.id)); })
+      .attr("d", path)
+      .on('mouseover', tip.show)
+       .on('mouseout', tip.hide);
+
+
+
+
+  svg.append("path")
+      .datum(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; }))
+      .attr("class", "states")
+      .attr("d", path);
+}
+
+var tip = d3.tip()
+  .attr('class', 'd3-tip')
+  .offset([140, 140])
+  .html(function(d) {
+   
+    return "<div style='opacity:0.8;font-family:monospace;padding:8px;;color:white'>"+
+            "<b>" + County.get(d.id) + ", " + State.get(d.id) + "</b><br/><br/>" + 
+            "Percentage of Population Cases: " + d3.format("0.2f")(Perc_Infected.get(d.id)) + "%<br/>" + 
+            "Number of Cases: " + Cases.get(d.id) + "<br/>" + 
+            "County Population: " + Population.get(d.id) 
+
+            "</div>";
+  })
+  g.call(tip);
+
+
+}
+
+
+  
+
---
+++ vector_plot/scatterplot.js
@@ -0,0 +1,121 @@
+
+
+// set the dimensions and margins of the graph
+var margin_scatter = {top: 50, right: 20, bottom: 60, left: 40},
+    width_scatter = 450 - margin_scatter.left - margin_scatter.right,
+    height_scatter = 300 - margin_scatter.top - margin_scatter.bottom;
+
+// append the svg object to the body of the page
+var svg2 = d3.select("#scatterplot")
+  .append("svg")
+    .attr("width", width_scatter + margin_scatter.left + margin_scatter.right)
+    .attr("height", height_scatter + margin_scatter.top + margin_scatter.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin_scatter.left + "," + margin_scatter.top + ")");
+
+svg2
+  .append("rect")
+    .attr("x",0)
+    .attr("y",0)
+    .attr("height", height_scatter)
+    .attr("width", width_scatter)
+    .style("fill", "EBEBEB")
+
+svg2.append("text")
+    .attr("x", (width_scatter / 2))             
+    .attr("y", 0 - (margin_scatter.top / 2))
+    .attr("text-anchor", "middle")  
+    .style("font-size", "14px") 
+    .style("text-decoration", "underline")  
+    .text("County Size vs Cases per Capita");
+
+//Read the data
+d3.csv("https://raw.githubusercontent.com/hannahjordanUM/Data_Vis_Repo/master/VectorPlot/covid_case_scatter.csv", function(data) {
+
+  // Add X axis
+  var x = d3.scaleLinear()
+    .domain([-20, 3500])
+    .range([ 0, width_scatter ]);
+
+  svg2.append("g")
+    .attr("transform", "translate(0," + height_scatter + ")")
+    .call(d3.axisBottom(x).tickSize(-height_scatter).ticks(10))
+    .select(".domain").remove()
+  // Add Y axis
+  var y = d3.scaleLinear()
+    .domain( [0,18])
+    .range([ height_scatter, 0 ]);
+  svg2.append("g")
+    .call(d3.axisLeft(y).tickSize(-width_scatter).ticks(7))
+    .select(".domain").remove()
+
+  svg2.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 0 - margin_scatter.left)
+    .attr("x",0 - (height_scatter / 2))
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .style("font-size","12px")
+    .text("Percentage of Population Infected");
+
+    svg2.append("text")
+    .attr("y", (width_scatter/2) + 10)
+    .attr("x", width_scatter/2)
+    .attr("dy", "1em")
+    .style("text-anchor", "middle")
+    .style("font-size","12px")
+    .text("County Population (in thousands)");
+
+    svg2.selectAll(".tick line").attr("stroke", "white")
+
+  
+    var tooltip = d3.select("#scatterplot")
+        .append("div")
+        .style("opacity", 0)
+        .attr("class", "d3-tip")
+
+
+
+
+      // A function that change this tooltip when the user hover a point.
+      // Its opacity is set to 1: we can now see it. Plus it set the text and position of tooltip depending on the datapoint (d)
+      var mouseover = function(d) {
+        tooltip
+          .style("opacity", 1)
+      }
+
+      var mousemove = function(d) {
+        tooltip
+          .html("Population: " + d3.format(",.2r")(d.population) + "  Percentage Cases: " + d3.format(".2f")(d.cases_per_pop) + "%")
+          .style("left", (d3.mouse(this)[0]-10) + "px") // It is important to put the +90: other wise the tooltip is exactly where the point is an it creates a weird effect
+          .style("top", (d3.mouse(this)[1]) + "px")
+      }
+
+      // A function that change this tooltip when the leaves a point: just need to set opacity to 0 again
+      var mouseleave = function(d) {
+        tooltip
+          .transition()
+          .duration(200)
+          .style("opacity", 0)
+      }
+
+
+// Add dots
+  svg2.append('g')
+    .selectAll("dot")
+    .data(data)
+    .enter()
+    .append("circle")
+      .attr("cx", function (d) { return x(d.pop_thousand); } )
+      .attr("cy", function (d) { return y(d.cases_per_pop); } )
+      .attr("r", 1.75)
+      .style("fill", "#006666")
+
+      .on("mouseover", mouseover)
+      .on("mousemove", mousemove)
+      .on("mouseleave", mouseleave);
+            
+
+})
+
